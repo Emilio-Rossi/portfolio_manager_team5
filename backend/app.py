d8c5723a5d78ef6fda851a4d7d2ed7d202f61c20
Emilio Rossi-Levin <emiliorossi-levin@Emilios-MacBook-Air.local>
2025-07-31 19:11:39 -0400

    register the search blueprint in app.py

--- backend/app.py
+++ backend/app.py
@@ -4,12 +4,14 @@ from dotenv import load_dotenv
 from pydantic import BaseModel, Field, validator
 from routes.portfolio_endpoint import portfolio_bp
 from routes.yfinance_endpoint import yfinance_bp
+from routes.search_endpoint import search_bp
 from models import *
 from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(portfolio_bp)
 app.register_blueprint(yfinance_bp)
+app.register_blueprint(search_bp)
 CORS(app)
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
